4ffc7d7c79b7b147b5c98d54592aa3936d9bbd63
PastorRowan <rowanvanzyl@gmail.com>
2026-09-22 17:06:53 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
+
 # ThePirateBayAPI
-ThePirateBayAPI
+
+A small Python project for experimenting with making HTTP requests to [The Pirate Bay](https://thepiratebay.org/index.html) API using Python's built-in urllib.request.
+
+The program:
+
+- Sends a search request for a specified query.
+- Requests the response as JSON.
+- Parses the JSON response.
+- Saves the response as a pretty-printed response.json file.
+
+## Requirements
+
+- [Python 3.13+](https://www.python.org/downloads/)
+- **No external dependencies.**
+
+## Usage
+
+Run:
+```
+python main.py
+```
+
+The default query is:
+```
+ubuntu
+```
+
+The resulting JSON response is written to:
+```
+response.json
+```
+
+## Technologies
+
+- [Python](https://www.python.org/)
+- [urllib](https://docs.python.org/3/library/urllib.html)
+- [pprint](https://docs.python.org/3/library/pprint.html)
+- [json](https://docs.python.org/3/library/json.html)
---
+++ main.py
@@ -0,0 +1,53 @@
+
+import urllib.request
+from pprint import pprint
+import json
+
+def main():
+
+    RESPONSE_FILE_NAME: str = "response.json"
+
+    QUERY: str = "ubuntu"
+
+    URL: str = (
+        "https://apibay.org/q.php?"
+        + urllib.parse.urlencode({"q": QUERY})
+    )
+
+    HEADERS: dict[str, str] = {
+        "Accept": "application/json",
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    REQUEST: urllib.request.Request = urllib.request.Request(
+        url=URL,
+        headers=HEADERS
+    )
+
+    print()
+    print(f"Requesting '{URL}' with headers:")
+    pprint(HEADERS)
+    print()
+
+    with urllib.request.urlopen(
+        url=REQUEST
+    ) as response:
+
+        print("Parsing json response")
+        print()
+
+        data = json.load(response)
+
+        print(f"Dumping pretty json response into '{RESPONSE_FILE_NAME}'")
+        print()
+
+        with open(
+            file=RESPONSE_FILE_NAME,
+            mode="w"
+        ) as response_json_file:
+            json.dump(data, response_json_file, indent=4)
+
+    print(f"Successfully made request to '{URL}' and dumped response into '{RESPONSE_FILE_NAME}'")
+
+if __name__ == "__main__":
+    main()
